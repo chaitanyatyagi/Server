304296258de2d82f7bb2de7d33b021fe38a20fd7
Chaitanya Tyagi <chaitanyatyagi1540@gmail.com>
2022-07-22 21:31:34 +0530

    done

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = require('./app');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const PORT = process.env.PORT || 4000;
+// const PORT = process.env.PORT || 4000;
 
 dotenv.config({
   path: './config.env',
@@ -37,13 +37,13 @@ mongoose.connection.on('error', (err) => {
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join('frontend/build')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.solve(__dirname, 'frontend', 'build', 'index.html'));
-  });
-}
-
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join('frontend/build')));
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.solve(__dirname, 'frontend', 'build', 'index.html'));
+//   });
+// }
+PORT = process.env.PORT || 4000;
 app.listen(PORT, '127.0.0.1', () => {
   console.log(`Server is listening on port ${PORT}`);
 });
